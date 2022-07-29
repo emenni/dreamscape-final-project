46972d3e8d250c02d3d190239f1fb4531659d0aa
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-07-28 23:54:49 -0300

    feat: admin component

--- FrontEnd/react/SmartSellControlPanel.tsx
+++ FrontEnd/react/SmartSellControlPanel.tsx
@@ -1,20 +1,39 @@
-import React, { FC } from 'react'
+import { useEffect, useState } from 'react'
+import React from 'react'
 import { Layout, PageBlock } from 'vtex.styleguide'
+import { Combo } from './Combo';
 
-const SmartSellControlPanel: FC = () => {
-  
+
+
+function Smart() {
+  const [jsonRecebido, setJsonRecebido] = useState([])
+
+  const getJson = () => {
+    const input = require('./combos.json');
+    setJsonRecebido(input.combos)
+  }
+  useEffect(() => {
+    getJson()
+  }, [])
+  console.log('recebido', jsonRecebido);
+
+
   let textoExplicativo = "Abaixo estão listados alguns produtos identificados com alta correlação entre si (numero de vendas, idade do cliente, etc"
-  
   return (
     <>
-    <Layout>
-      <h1>Painel de Controle para solução Smart Sell</h1>
-      <PageBlock title="Analise de Product Matching" subtitle={textoExplicativo} variation="full">
-      </PageBlock>
-    </Layout>
-  </>
-  )  
+      <Layout>
+        <h1>Painel de Controle para solução Smart Sell</h1>
+        <PageBlock title="Analise de Product Matching" subtitle={textoExplicativo} variation="full">
+          <h3>Combos mais vendidos:</h3>
+          <div>
+            {jsonRecebido.length === 0 && <p>Carregando...</p>}
+            {jsonRecebido.length > 0 && jsonRecebido.map((combos) => <Combo key={combos.combinationId} combos={combos} />)
+            }
+          </div>
+        </PageBlock>
+      </Layout>
+    </>
+  )
 }
 
-export default SmartSellControlPanel
-
+export default Smart
