2f60ea7a9ad18855e819deaf90fea83ebee6bebf
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-08-02 13:40:41 -0300

    fix: smart sell component fixed

--- FrontEnd/react/SmartSellControlPanel.tsx
+++ FrontEnd/react/SmartSellControlPanel.tsx
@@ -1,20 +1,4 @@
-import React, { FC } from 'react'
-import { Layout, PageBlock } from 'vtex.styleguide'
+import { Smart } from './components/Smart'
 
-const SmartSellControlPanel: FC = () => {
-  
-  let textoExplicativo = "Abaixo estão listados alguns produtos identificados com alta correlação entre si (numero de vendas, idade do cliente, etc"
-  
-  return (
-    <>
-    <Layout>
-      <h1>Painel de Controle para solução Smart Sell</h1>
-      <PageBlock title="Analise de Product Matching" subtitle={textoExplicativo} variation="full">
-      </PageBlock>
-    </Layout>
-  </>
-  )  
-}
-
+const SmartSellControlPanel = Smart
 export default SmartSellControlPanel
-
