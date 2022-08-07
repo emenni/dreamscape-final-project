332bb73110197cc56b315ce8d59a643064b502c2
Michel Negrao <michelnegrao@gmail.com>
2022-08-07 14:38:03 -0300

    :fix - Alterando chamado do useCombinations para ao inves de usar map usar um endPoint

--- tools/canceladorDePedidos.js
+++ tools/canceladorDePedidos.js
@@ -3,21 +3,27 @@ const orderData = require('./cancelarPedidos.json')
 
     async function enviarPedido() {
 
-        orderData.Orders.forEach(async (record) => {
+        orderData.data.ordersExtendSearch.list.forEach(async (record) => {
 
+            if (record.status === "ready-for-handling") {
 
-            let response = await axios.post(`https://dreamscape.vtexcommercestable.com.br/api/oms/pvt/orders/${record.orderId}/cancel`,{},
-            {
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                    'X-VTEX-API-AppKey': 'vtexappkey-dreamscape-KARBSE',
-                    'X-VTEX-API-AppToken': 'OGAJZINNXDAIJRRATDIADXZQRWVLYXAVJVCVSWEBOUNZYWHDAAOUDZPBWVNSYDPZBHITVVGDEVDVQQKHHDQUEYHSFGKVHUYQVRZYACYIFZNEMUZCBFYXKYRFOBWSMKYJ'
-                },
-                            
+                
+                
+                let response = await axios.post(`https://dreamscape.vtexcommercestable.com.br/api/oms/pvt/orders/${record.orderId}/start-handling`,{},
+                
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        'X-VTEX-API-AppKey': 'vtexappkey-dreamscape-KARBSE',
+                        'X-VTEX-API-AppToken': 'OGAJZINNXDAIJRRATDIADXZQRWVLYXAVJVCVSWEBOUNZYWHDAAOUDZPBWVNSYDPZBHITVVGDEVDVQQKHHDQUEYHSFGKVHUYQVRZYACYIFZNEMUZCBFYXKYRFOBWSMKYJ'
+                    },
+                    
                 })
-
-            console.log('STATUS DA REQUISICAO:', record.orderId, response.data)
+                
+                console.log('STATUS DA REQUISICAO:', record.orderId, response.data)
+            
+            }
 
         })
 
